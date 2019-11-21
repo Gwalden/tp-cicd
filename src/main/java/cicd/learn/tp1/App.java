89e848c362b831c62ffa09cfdffa5b425db23fb3
Gwalden <leovinga@hotmail.fr>
2019-11-21 11:59:07 +0100

    add updateMessage

--- src/main/java/cicd/learn/tp1/App.java
+++ src/main/java/cicd/learn/tp1/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! UPDATE" );
+        System.out.println( "Hello World! UPDATE ci/cd" );
     }
 }
